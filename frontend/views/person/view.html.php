57b8c96c41881c67f6fbe1359a0586f5da8ab252
momo_102 <momo_102@c8c2f11a-2b74-0410-b686-9bf4fdd139bc>
2009-09-23 12:08:25 +0000

    Initial commit

---
+++ frontend/views/person/view.html.php
@@ -0,0 +1,53 @@
+<?php
+/**
+* @version		0.5
+* @package		Joomla
+* @subpackage	ClubManagement-Membership
+* @copyright	Copyright (c) 2009 Norbert Kümin. All rights reserved.
+* @license		http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE
+* @author		Norbert Kuemin
+* @authorEmail	momo_102@bluemail.ch
+*/
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+
+jimport( 'joomla.application.component.view' );
+require_once( JPATH_COMPONENT_ADMINISTRATOR.DS.'classes'.DS.'nokCMPerson.php');
+
+class cmViewperson extends JView
+{
+	var $user;
+	var $document;
+	var $params;
+	var $params_menu;
+	var $header;
+	var $data;
+	var $menu;
+	var $cmobject;
+
+	function display($tpl = null)
+	{
+		global $mainframe;
+		
+		/*
+		 * Init variables
+		 */
+		$this->user		=& JFactory::getUser();
+		$this->document	=& JFactory::getDocument();
+		$this->params	=& $mainframe->getParams();
+		$menus	= &JSite::getMenu();
+		$this->menu	= $menus->getActive();
+		$this->cmobject = new nokCMPerson("com_clubmanagement");
+		if (is_object( $this->menu )) {
+			$this->params_menu = new JParameter( $this->menu->params );
+		}
+
+		/*
+		 * Init document
+		 */
+		$this->document->setMetaData('robots', 'noindex, nofollow');
+		parent::display($tpl);
+    }
+}
+?>
