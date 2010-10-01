edd3719212c0d4b57118034a859fc5141f87b8a7
momo_102 <momo_102@c8c2f11a-2b74-0410-b686-9bf4fdd139bc>
2010-10-01 13:45:03 +0000

    Email-Addresslist

--- trunk/com_clubmanagement/backend/elements/cmmembertype.php
+++ trunk/com_clubmanagement/backend/elements/cmmembertype.php
@@ -30,10 +30,33 @@ class JElementCMMemberType extends JElement
 		$cmobject = new nokCMMembership("com_clubmanagement");
 		$arrList = $cmobject->getMemberTypes();
 		$opt[] = JHTML::_("select.option",  "*", JText::_("ALL"));
+		$ctrl = $control_name.'['.$name.']';
+		$multi = false;
+
+		// Construct the various argument calls that are supported.
+		$attribs = '';
+		if ($v = $node->attributes( 'size' )) {
+				$attribs .= 'size="'.$v.'" ';
+		}
+		if ($v = $node->attributes( 'class' )) {
+				$attribs       .= 'class="'.$v.'" ';
+		} else {
+				$attribs       .= 'class="inputbox" ';
+		}
+		if (($v = $node->attributes( 'multiple' )) && ($v == "true")) {
+			$attribs .= 'multiple ';
+			$ctrl .= '[]';
+			$multi = true;
+		}
+
 		reset($arrList);
 		while (list($strValue,$strDisplay) = each($arrList)) {
-			$opt[] = JHTML::_("select.option",  $strValue, $strDisplay);
+			if (($multi === true) && ($strValue == "*")) {
+				// Don't add ALL to multi select list
+			} else {
+				$opt[] = JHTML::_("select.option",  $strValue, $strDisplay);
+			}
 		}
-		return JHTML::_('select.genericlist', $opt, $control_name.'['.$name.']', "class=\"inputbox\"", "value", "text", $value);
+		return JHTML::_('select.genericlist', $opt, $ctrl, $attribs, "value", "text", $value);
 	}
 }
