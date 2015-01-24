6fac665e1ad345c6f94deffa57015cd6483c1cbe
momo_102 <momo_102@c8c2f11a-2b74-0410-b686-9bf4fdd139bc>
2015-01-24 12:41:04 +0000

    Create helper for emaillists Board email list

--- com_clubmanagement/site/views/memberships/tmpl/email.php
+++ com_clubmanagement/site/views/memberships/tmpl/email.php
@@ -11,95 +11,8 @@
 
 defined('_JEXEC') or die; // no direct access
 
-function display_link($name, $data, $target) {
-	switch ($target) {
-		case "cc":
-			$tfexpr = "?cc=";
-			break;
-		case "bcc":
-			$tfexpr = "?bcc=";
-			break;
-		default:
-			$tfexpr = "";
-			break;
-	}
-	$emails = array();
-	foreach ($data as $row) {
-		array_push($emails, $row['person_email']);
-	}
-	if (count($emails) > 0) {
-		echo "<a href=\"MAILTO:".$tfexpr.implode(",",$emails)."\">".$name."</a>\n";
-	} else {
-		echo JText::_("COM_CLUBMANAGEMENT_NO_DATA");
-	}
-}
-
-function display_email_link ($label, $data, $max_email_addr, $target_field) {
-	echo $label."\n";
-	if (($max_email_addr == 0) || ($max_email_addr >= count($data))) {
-		// One link
-		display_link(JText::_("COM_CLUBMANAGEMENT_LINK"), $data, $target_field);
-	} else {
-		// Multiple links
-		$max = intval(count($data) / $max_email_addr) + 1;
-		for ($i=1 ; $i<=$max ; $i++) {
-			$newdata = array_slice($data, (($i-1)*$max_email_addr), $max_email_addr);
-			display_link(JText::sprintf("COM_CLUBMANAGEMENT_LINK_NR",$i), $newdata, $target_field);
-		}
-	}
-	echo "<br/>\n";
-}
-
-function calculateKey($row, $mode) {
-	if ($mode == 'member') {
-		$key = $row['member_type'];
-		if (($row['member_end'] <> '') && ($row['member_end'] <> '0000-00-00')) {
-			$key .= '_terminated';
-		} else {
-			$key .= '_current';
-		}
-		return $key;
-	}
-	if ($mode == 'board') {
-		if (($row['board_end'] <> '') && ($row['board_end'] <> '0000-00-00')) {
-			return 'terminated';
-		} else {
-			return 'current';
-		}
-	}
-}
-
-function splitdata ($items, $published, $mode) {
-	$result = array();
-	if ($items) {
-		foreach($items as $item) {
-			$row = (array) $item;
-			$key = calculateKey($row, $mode);
-			if ($row['person_email']) {
-				switch ($mode) {
-					case "member":
-						$rowPublished = $row['member_published'];
-						break;
-					case "board":
-						$rowPublished = $row['board_published'];
-						break;
-					default:
-						$rowPublished = '0';
-						break;
-				}
-				if (($published == "all") ||
-				   ($published == 'unpublished' && $rowPublished == '0') ||
-				   ($published == 'published' && $rowPublished == '1')) {
-					if (!$result[$key]) {
-						$result[$key] = array();
-					}
-					array_push($result[$key], $row);
-				}
-			}
-		}
-	}
-	return $result;
-}
+JLoader::register('EmailListHelper', JPATH_COMPONENT_ADMINISTRATOR.'/helpers/emaillist.php', true);
+JLoader::register('SelectionHelper', JPATH_COMPONENT_ADMINISTRATOR.'/helpers/selection.php', true);
 
 function getMemberType($type, $memberTypes) {
 	if ($memberTypes[$type]) {
@@ -108,22 +21,6 @@ function getMemberType($type, $memberTypes) {
 	return $type;
 }
 
-function getStates($state) {
-	$states = array();
-	switch ($state) {
-		case "terminated": // Only terminated
-			$states[]="terminated";
-			break;
-		case "current": // Only Current
-			$states[]="current";
-			break;
-		default: // All
-			$states[]="terminated";
-			$states[]="current";
-			break;
-	}
-	return $states;
-}
 
 /*
  * Get parameters
@@ -131,8 +28,6 @@ function getStates($state) {
 $memberType = $this->paramsMenuEntry->get("membertype");
 $memberPublicity = $this->paramsMenuEntry->get("memberpublicity");
 $memberState = $this->paramsMenuEntry->get("memberstate");
-$boardPublicity = $this->paramsMenuEntry->get("memberpublicity");
-$boardState = $this->paramsMenuEntry->get("memberstate");
 $maxEmailAddr = $this->paramsMenuEntry->get("max_email_addr");
 $targetField = $this->paramsMenuEntry->get("target_field");
 
@@ -141,30 +36,19 @@ $targetField = $this->paramsMenuEntry->get("target_field");
  */
 $cols = array();
 $cols[] = "email";
-$mstate = getStates($memberState);
-$bstate = getStates($boardState);
+$states = EmailListHelper::getStates($memberState);
 if (!is_array($memberType)) {
 	$temp = $memberType;
 	$memberType = array();
 	$memberType[0] = $temp;
 }
 reset($memberType);
-JLoader::register('SelectionHelper', JPATH_COMPONENT_ADMINISTRATOR.'/helpers/selection.php', true);
 $memberTypes = SelectionHelper::getSelection("member_types");
-$data = splitdata($this->items, $memberPublicity,'member');
+$data = EmailListHelper::splitdata($this->items, $memberPublicity,'member');
 foreach($memberType as $type) {
-	foreach($mstate as $state) {
-		display_email_link (getMemberType($type, $memberTypes)." (".JText::_($state)."):", $data[$type.'_'.$state], $maxEmailAddr, $targetField);
+	foreach($states as $state) {
+		EmailListHelper::display_email_link (getMemberType($type, $memberTypes)." (".JText::_($state)."):", $data[$type.'_'.$state], $maxEmailAddr, $targetField);
 	}
 }
-/*
-if ($this->paramsMenuEntry->get('boardshow') == '1') {
-	$modelBoard = $this->getModel( 'boardentries' );
-	$data = splitdata($modelBoard->get('items'), $boardPublicity,'board');
-	foreach($bstate as $state) {
-		display_email_link (JText::_("COM_CLUBMANAGEMENT_BOARD")." (".JText::_($state)."):", $data[$state], $maxEmailAddr, $targetField);
-	}
-}
-*/
 ?>
 
