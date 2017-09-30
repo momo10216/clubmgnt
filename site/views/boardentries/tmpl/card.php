2d7ed87df8e4d142f9e344ab7e42d98e16b0a945
momo_102@bluemail.ch <Norbert Kuemin>
2017-09-30 21:46:57 +0100

    Refactor board card Add more image setting

--- site/views/boardentries/tmpl/card.php
+++ site/views/boardentries/tmpl/card.php
@@ -10,18 +10,43 @@
 */
 
 defined('_JEXEC') or die; // no direct access
+
 $FieldPerLine=4;
 $Line=5;
 $details = false;
-if ($this->paramsMenuEntry->get( 'detail_enable' ) != "0") {
+$EOL = "\n";
+
+function getImageDir($imageDir) {
+	if (!empty($imageDir)) {
+		if (substr($imageDir,-1) != DIRECTORY_SEPARATOR) {
+			$imageDir = $imageDir.DIRECTORY_SEPARATOR;
+		}
+		if (substr(JURI::root(),-1) == DIRECTORY_SEPARATOR) {
+			if (substr($imageDir,0,1) == DIRECTORY_SEPARATOR) {
+				$imageDir = JURI::root().substr($imageDir,1);
+			} else {
+				$imageDir = JURI::root().$imageDir;
+			}
+		} else {
+			if (substr($imageDir,0,1) == DIRECTORY_SEPARATOR) {
+				$imageDir = JURI::root().$imageDir;
+			} else {
+				$imageDir = JURI::root().DIRECTORY_SEPARATOR.$imageDir;
+			}
+		}
+	}
+	return $imageDir;
+}
+
+if ($this->paramsMenuEntry->get('detail_enable') != "0") {
 	JHtml::_('behavior.modal');
 	$details = true;
 	$curi =& JFactory::getURI();
 	$uri =& JURI::getInstance( $curi->toString() );
-	$uri->setVar("layout","detail");
-	$uri->setVar("tmpl","component");
-	$uri->setVar("Itemid","");
-	$uri->setVar("view","person");
+	$uri->setVar('layout','detail');
+	$uri->setVar('tmpl','component');
+	$uri->setVar('Itemid','');
+	$uri->setVar('view','person');
 }
 
 /*
@@ -29,7 +54,7 @@ if ($this->paramsMenuEntry->get( 'detail_enable' ) != "0") {
  */
 $cols = array();
 for ($i=1;$i<=20;$i++) {
-	$field = "column_".$i;
+	$field = 'column_'.$i;
 	$cols[] = $this->paramsMenuEntry->get($field);
 }
 $colcount = count($cols);
@@ -37,45 +62,38 @@ $colcount = count($cols);
 /*
  * Display
  */
-$imageDir = $this->paramsComponent->get('image_dir');
-if (!empty($imageDir)) {
-	if (substr($imageDir,-1) != DIRECTORY_SEPARATOR) {
-		$imageDir = $imageDir.DIRECTORY_SEPARATOR;
-	}
-	if (substr(JURI::root(),-1) == DIRECTORY_SEPARATOR) {
-		if (substr($imageDir,0,1) == DIRECTORY_SEPARATOR) {
-			$imageDir = JURI::root().substr($imageDir,1);
-		} else {
-			$imageDir = JURI::root().$imageDir;
-		}
-	} else {
-		if (substr($imageDir,0,1) == DIRECTORY_SEPARATOR) {
-			$imageDir = JURI::root().$imageDir;
-		} else {
-			$imageDir = JURI::root().DIRECTORY_SEPARATOR.$imageDir;
-		}
-	}
+$cssText = '';
+switch ($this->paramsMenuEntry->get('border_type')) {
+	case 'row':
+		$cssText .= '.cmTableBorder { border-top-style:solid; border-width:1px; }'.$EOL;
+		break;
+	case 'grid':
+		$cssText .= '.cmTableBorder { border-style:solid; border-width:1px; }'.$EOL;
+		break;
+	default:
+		$cssText .= '.cmTableBorder {  }'.$EOL;
+		break;
 }
-if ($this->paramsMenuEntry->get( "table_center") == "1") echo "<center>\n";
+$cssText .= '.cmTableCellTextAlign { text-align: '.$this->paramsMenuEntry->get('textalign').'; }'.$EOL;
+$imgSize = intval($this->paramsMenuEntry->get('image_size_value'));
+if ($imgSize > 0) {
+	if (($this->paramsMenuEntry->get('image_size_uom') == '%') && ($imgSize >100)) { $imgSize = 100; }
+	$cssText .= '.cmTableImage { width: '.$imgSize.$this->paramsMenuEntry->get('image_size_uom').'; height: auto; }'.$EOL;
+} else {
+	$cssText .= '.cmTableImage { }'.$EOL;
+}
+$doc = JFactory::getDocument();
+$doc->addStyleDeclaration($cssText);
+$imageDir = getImageDir($this->paramsComponent->get('image_dir'));
+if ($this->paramsMenuEntry->get('table_center') == '1') echo '<center>'.$EOL;
 if ($this->items) {
 	JLoader::register('SelectionHelper', JPATH_COMPONENT_ADMINISTRATOR.'/helpers/selection.php', true);
-	$boardJobs = SelectionHelper::getSelection("board_jobs");
-	switch ($this->paramsMenuEntry->get( "border_type")) {
-		case "row":
-			$borderStyle = " style=\"border-top-style:solid; border-width:1px\"";
-			break;
-		case "grid":
-			$borderStyle = " style=\"".$border."\"";
-			break;
-		default:
-			$borderStyle = "";
-			break;
-	}
+	$boardJobs = SelectionHelper::getSelection('board_jobs');
+	$imageCol = $this->paramsMenuEntry->get('column_image');
 	foreach($this->items as $item) {
 		$row = (array) $item;
 		$lines = array();
-		$imageCol = $this->paramsMenuEntry->get( "column_image" );
-		if ($imageCol != "") {
+		if ($imageCol != '') {
 			$image = $row[$imageCol];
 		}
 		for($i=0;$i<$Line;$i++) {
@@ -84,92 +102,92 @@ if ($this->items) {
 				$colnr = $i*$FieldPerLine+$j;
 				$field = $cols[$colnr];
 				$data = '';
-				if (($field == "board_job") && !empty($boardJobs[$row[$field]])) {
+				if (($field == 'board_job') && !empty($boardJobs[$row[$field]])) {
 					$data = $boardJobs[$row[$field]];
 				} else {
 					if (isset($row[$field])) { $data = $row[$field]; }
 				}
 
 				if (strlen($data) > 0) {
-					if ($lines[$i]) { $lines[$i] .= " "; }
-					if ($details && ($this->paramsMenuEntry->get( 'detail_column_link' ) == $field) && ($data != "")) {
-						$data = "<a href=\"".$uri->toString()."\" class=\"modal\" rel=\"{handler: 'iframe', size: {x: ".$this->paramsMenuComponent->get( 'detail_width' ).", y: ".$this->paramsComponent->get( 'detail_height' )."}}\">".$data."</a>";
+					if ($lines[$i]) { $lines[$i] .= ' '; }
+					if ($details && ($this->paramsMenuEntry->get('detail_column_link') == $field) && ($data != '')) {
+						$data = '<a href="'.$uri->toString().'" class="modal" rel="{handler: \'iframe\', size: {x: '.$this->paramsMenuComponent->get('detail_width').', y: '.$this->paramsComponent->get('detail_height').'}}">'.$data.'</a>';
 					}
 					$lines[$i] .= $data;
 					$lines[$i] = trim($lines[$i]);
 				}
 			}
-			if ($details && ($this->paramsMenuEntry->get( 'detail_column_link' ) == "") && ($data != "")) {
-				$lines[$i] = "<a href=\"".$uri->toString()."\" class=\"modal\" rel=\"{handler: 'iframe', size: {x: ".$this->paramsComponent->get( 'detail_width' ).", y: ".$this->paramsComponent->get( 'detail_height' )."}}\">".$lines[$i]."</a>";
+			if ($details && ($this->paramsMenuEntry->get('detail_column_link') == '') && ($data != '')) {
+				$lines[$i] = '<a href="'.$uri->toString().'" class="modal" rel="{handler: \'iframe\', size: {x: '.$this->paramsComponent->get('detail_width').', y: '.$this->paramsComponent->get('detail_height').'}}">'.$lines[$i].'</a>';
 			}
 		}
-		echo "<table".$borderStyle.">\n";
-		switch ($this->paramsMenuEntry->get( "picpos" )) {
-			case "right": //picture on the left side
-				echo "<tr valign=\"top\">\n";
-				echo "<td align=\"".$this->paramsMenuEntry->get( "textalign" )."\">";
+		echo '<table class="cmTableBorder">'.$EOL;
+		switch ($this->paramsMenuEntry->get('picpos')) {
+			case 'right': //picture on the right side
+				echo '<tr valign="top">'.$EOL;
+				echo '<td class="cmTableCellTextAlign">';
 				for($i=0;$i<$Line;$i++) {
 					if (strlen($lines[$i]) > 0) {
-						echo $lines[$i]."<br />\n";
+						echo $lines[$i].'<br />';
 					}
 				}
-				echo "</td>\n";
+				echo '</td>'.$EOL;
 				if ($image) {
-					echo "<td align=\"".$this->paramsMenuEntry->get( "textalign" )."\"><img src=\"".$imageDir.$image."\" width=\"".$this->paramsMenuEntry->get('image_size')."\"></td>\n";
+					echo '<td class="cmTableCellTextAlign"><img src="'.$imageDir.$image.'" class="cmTableImage"></td>'.$EOL;
 				}
-				echo "</tr>\n";
+				echo '</tr>'.$EOL;
 				break;
-			case "top": //picture on the left side
-				echo "<tr valign=\"top\">\n";
+			case 'top': //picture on top
+				echo '<tr valign="top">'.$EOL;
 				if ($image) {
-					echo "<td align=\"".$this->paramsMenuEntry->get( "textalign" )."\"><img src=\"".$imageDir.$image."\" width=\"".$this->paramsMenuEntry->get('image_size')."\"></td>\n";
+					echo '<td class="cmTableCellTextAlign"><img src="'.$imageDir.$image.'" class="cmTableImage"></td>'.$EOL;
 				}
-				echo "</tr>\n";
-				echo "<tr valign=\"top\">\n";
-				echo "<td align=\"".$this->paramsMenuEntry->get( "textalign" )."\">";
+				echo '</tr>'.$EOL;
+				echo '<tr valign="top">'.$EOL;
+				echo '<td class="cmTableCellTextAlign">';
 				for($i=0;$i<$Line;$i++) {
 					if (strlen($lines[$i]) > 0) {
-						echo $lines[$i]."<br />\n";
+						echo $lines[$i].'<br />';
 					}
 				}
-				echo "</td>\n";
-				echo "</tr>\n";
+				echo '</td>'.$EOL;
+				echo '</tr>'.$EOL;
 				break;
-			case "bottom": //picture on the left side
-				echo "<tr valign=\"top\">\n";
-				echo "<td align=\"".$this->paramsMenuEntry->get( "textalign" )."\">";
+			case 'bottom': //picture on the bottom
+				echo '<tr valign="top">'.$EOL;
+				echo '<td class="cmTableCellTextAlign">';
 				for($i=0;$i<$Line;$i++) {
 					if (strlen($lines[$i]) > 0) {
-						echo $lines[$i]."<br />\n";
+						echo $lines[$i].'<br />';
 					}
 				}
-				echo "</td>\n";
-				echo "</tr>\n";
-				echo "<tr valign=\"top\">\n";
+				echo '</td>'.$EOL;
+				echo '</tr>'.$EOL;
+				echo '<tr valign="top">'.$EOL;
 				if ($image) {
-					echo "<td align=\"".$this->paramsMenuEntry->get( "textalign" )."\"><img src=\"".$imageDir.$image."\" width=\"".$this->paramsMenuEntry->get('image_size')."\"></td>\n";
+					echo '<td align=class="cmTableCellTextAlign"><img src="'.$imageDir.$image.'" class="cmTableImage"></td>'.$EOL;
 				}
-				echo "</tr>\n";
+				echo '</tr>'.$EOL;
 				break;
-			case "left": //picture on the left side
+			case 'left': //picture on the left side
 			default:
-				echo "<tr valign=\"top\">\n";
+				echo '<tr valign="top">'.$EOL;
 				if ($image) {
-					echo "<td align=\"".$this->paramsMenuEntry->get( "textalign" )."\"><img src=\"".$imageDir.$image."\" width=\"".$this->paramsMenuEntry->get('image_size')."\"></td>\n";
+					echo '<td class="cmTableCellTextAlign"><img src="'.$imageDir.$image.'" class="cmTableImage"></td>'.$EOL;
 				}
-				echo "<td align=\"".$this->paramsMenuEntry->get( "textalign" )."\">";
+				echo '<td class="cmTableCellTextAlign">';
 				for($i=0;$i<$Line;$i++) {
 					if (strlen($lines[$i]) > 0) {
-						echo $lines[$i]."<br />\n";
+						echo $lines[$i].'<br />';
 					}
 				}
-				echo "</td>\n";
-				echo "</tr>\n";
+				echo '</td>'.$EOL;
+				echo '</tr>'.$EOL;
 				break;
 		}
-		echo "</table>\n";
-		echo "<p/>\n";
+		echo '</table>'.$EOL;
+		echo '<p/>'.$EOL;
 	}
 }
-if ($this->paramsMenuEntry->get( "table_center") == "1") echo "</center>\n";
+if ($this->paramsMenuEntry->get('table_center') == '1') echo '</center>'.$EOL;
 ?>
