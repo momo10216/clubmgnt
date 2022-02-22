5aa7a92f8c4afd7f052e99172681e066f7e451a3
Norbert Kuemin <momo_102@bluemail.ch>
2022-02-22 09:52:45 +0100

    Change tables for correct encoding

--- admin/sql/updates/mysql/3.0.22.sql
+++ admin/sql/updates/mysql/3.0.22.sql
@@ -1,4 +1,4 @@
-#ALTER TABLE `#__nokCM_persons` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-#ALTER TABLE `#__nokCM_memberships` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-#ALTER TABLE `#__nokCM_board` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `#__nokCM_persons` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `#__nokCM_memberships` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `#__nokCM_board` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
